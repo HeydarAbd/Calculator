58736e05a75ad8dc6462fdc10ed7ca120fedc8b0
Heydar Abdullayev <43150761+HeydarAbd@users.noreply.github.com>
2019-01-19 13:03:33 -0500

    Add multiplyagain method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -17,6 +17,11 @@ public class Calculator {
     public static double multiply(double num1, double num2){
         return num1*num2;
     }
+    
+    
+    public static double multiplyagain(double num1, double num2){
+            return num1*num2*num1;
+    }
 
 
 
